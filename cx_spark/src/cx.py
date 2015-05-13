e444c3d2ba8aa63d862c2044d81c7b8b15ddb552
Jiyan Yang <jiyanyang12@gmail.com>
2015-05-12 22:28:39 -0700

    editing samp_eval_err.py

--- cx_spark/src/cx.py
+++ cx_spark/src/cx.py
@@ -36,7 +36,7 @@ class CX:
         return lev_row, lev_col, p_row, p_col
 
     def comp_idx(self, p, scheme='deterministic', r=10):
-        #seleting rows based on self.lev
+        #seleting rows based on p
         #scheme can be either 'deterministic' or 'randomized'
         #r dentotes the number of rows to select
 
@@ -48,65 +48,46 @@ class CX:
 
         return self.idx
 
-    def get_rows(self):
-        #getting the selected rows back
-        idx = self.idx
-        rows = self.matrix_A.rdd.filter(lambda (key, row): key in idx).collect()
-        self.R = np.array([row[1] for row in rows])
-
-        return self.R.shape #shape of R is r by d
-
-    def comp_err(self, R=None):
-        #computing the reconstruction error
-        #Rinv = np.linalg.pinv(self.R) #its shape is d by r
-        #RRinv = np.dot(Rinv, self.R) #its shape is d by d
-        #temp = np.eye(self.matrix_A.n) - RRinv
+    def get_rows(self, idx):
 
         n = self.matrix_A.n
 
-        if R is None:
-            idx = self.idx
-            rows = self.matrix_A.rdd.filter(lambda (key, row): key in idx).collect()
-            #self.R = np.array([row[1] for row in rows])
+        #getting the selected rows back
+        rows = self.matrix_A.rdd.filter(lambda (key, row): key in idx).collect()
+        
+        #self.R = np.array([row[1] for row in rows])
+        data = {'row':[], 'col':[], 'val':[]}
+        i = 0
+        for row in rows:
+            data['row'] += [i]*len(row[1][0])
+            data['col'] += row[1][0].tolist()
+            data['val'] += row[1][1].tolist()
+            i += 1
 
-            data = {'row':[], 'col':[], 'val':[]}
-            row_idx = 0
-            for row in rows:
-                data['row'] += [row_idx]*len(row[1][0])
-                data['col'] += row[1][0].tolist()
-                data['val'] += row[1][1].tolist()
-                row_idx += 1
+        R = form_csr_matrix(data,len(rows),n).toarray()
 
-            R = form_csr_matrix(data,len(rows),n).toarray()
-            np.savetxt('temp_R.txt', R)
+        print R.shape
 
+        return R  #shape of R is r by d
+
+    def comp_err(self, R):
+        #computing the reconstruction error
+
+        n = self.matrix_A.n
         print R.shape
 
         # assuming the matrix is tall
         U, D, V = np.linalg.svd(R, full_matrices=False)
 
         print V.shape # shape of V should be r by d
-
         V = self.matrix_A.rdd.context.broadcast(V)
 
-        #diff = np.sqrt( self.matrix_A.rtimes(temp,self.sc,True).map(lambda (key,row): norm(row)**2 ).sum() )
-        #sum_norms = self.matrix_A.rdd.map(lambda row: comp_norm_mapper(row, V.value, n)).sum()
         cnm = comp_norm_mapper()
         sum_norms = self.matrix_A.rdd.mapPartitions(lambda records: cnm(records, V=V.value, n=n)).sum()
 
-        # do it in mappartitions?
+        relative_err = np.sqrt( sum_norms[1]/sum_norms[0] )
 
-        return sum_norms
-
-#def comp_norm_mapper(row, V, n):
-#    arr = np.zeros(n)
-#    arr[row[1][0]] = row[1][1] # densify the row
-#
-#    a = np.linalg.norm(arr)**2
-#    b = np.linalg.norm(arr - np.dot( np.dot(arr, V.T), V ))**2
-#
-#    return np.array([a,b])
-    #return a
+        return relative_err
 
 class comp_norm_mapper(BlockMapper):
 
@@ -126,7 +107,7 @@ class comp_norm_mapper(BlockMapper):
         data = form_csr_matrix(self.data,len(self.keys),n).toarray()
 
         a = np.linalg.norm(data, 'fro')**2
-        b = np.linalg.norm( np.dot( np.dot(data, V.T), V), 'fro')**2
+        b = np.linalg.norm( data - np.dot( np.dot(data, V.T), V), 'fro')**2
 
         self.results += np.array([a,b])
 
@@ -134,6 +115,4 @@ class comp_norm_mapper(BlockMapper):
 
     def close(self):
         yield self.results
-        #for r in emit_results(self.atamat, self.direct_sum, axis=0):
-        #    yield r
 
--- cx_spark/src/samp_eval_err.py
+++ cx_spark/src/samp_eval_err.py
@@ -5,9 +5,7 @@ from utils import prepare_matrix
 from cx import CX
 from sparse_row_matrix import SparseRowMatrix
 
-conf = SparkConf().set('spark.eventLog.enabled','true')
-#.set('spark.driver.maxResultSize', '15g').set("spark.executor.memory", "30g")
-sc = SparkContext(appName='samp_eval_err',conf=conf)
+import os
 import ast
 import numpy as np
 import logging.config
@@ -16,39 +14,61 @@ import logging
 logging.config.fileConfig('logging.cfg', disable_existing_loggers=False) 
 logger = logging.getLogger(__name__)
 
+conf = SparkConf().set('spark.eventLog.enabled','true')
+#.set('spark.driver.maxResultSize', '15g').set("spark.executor.memory", "30g")
+sc = SparkContext(appName='samp_eval_err',conf=conf)
+
 def parse(string):
     s = str(string)
     val = ast.literal_eval(s)
     return val[0], (np.array(val[1][0]), np.array(val[1][1]))
 
+
+
+
+logger.info('----------------------------------------')
+logger.info('Ready to do CX (sampling and computing reconstruction error)!')
+
 data = sc.textFile('msi_data',500).map(lambda x:parse(x))
 
-#row_shape = 131048
-#column_shape = 8258911
-#131047 8258910
 row_shape = 8258911
 column_shape = 131048
-#column_shape+=20
-
-#print data.take(1)
-#print data.count()
 
 matrix_A = SparseRowMatrix(data, 'output', row_shape, column_shape, False)
 
 #row_lev = np.loadtxt( '../results/row_leverage_scores_logged' )
+
+logger.info('Loading p values!')
 row_p = np.loadtxt( '../results/row_p_scores_logged' )
-print row_p.shape
+logger.info('Finish loading! Its shape is {0}'.format(row_p.shape))
 
 cx = CX(matrix_A)
 
-#idx = cx.comp_idx(row_p, scheme='deterministic', r=50)
-#print idx
+scheme = 'deterministic'
+r = 20
 
-R = np.loadtxt('temp_R.txt')
+R_filename = 'R_files/' + 'R_' + scheme + '_' + str(r) + '.txt'
 
-sum_norms = cx.comp_err(R)
+if os.path.isfile(R_filename):
+    logger.info('Found existing R file, loading them!')
+    R = np.loadtxt(R_filename)
 
-print sum_norms
+else:
+	logger.info('Getting row indices!')
+	logger.info('Using {0}, number of rows to sample is {1}'.format( scheme, r ))
+	idx = cx.comp_idx(row_p, scheme = scheme, r = r)
+	logger.info('Indices are {0}!'.format(idx))
+
+	logger.info('Getting rows!')
+	R = cx.get_rows(idx)
+	logger.info('Saving rows!')
+	R = np.savetxt(R_filename)
+
+logger.info('Computing relative err using R!')
+relative_err = cx.comp_err(R)
+logger.info('Relative error is {0}!'.format(relative_err))
+
+
 
 
 
